11186ff2b6ad1e39a08c42fe8b7cdcff3f59fc8e
makutak <kounotakuma@realglobe.jp>
2016-01-24 17:19:23 +0900

    チャットスライダーを拡大、収納させる

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -26,12 +26,17 @@ spa.shell = (function () {
             +'</div>'
             +'<div class="spa-shell-foot"></div>'
             +'<div class="spa-shell-chat"></div>'
-            +'<div class="spa-shell-modal"></div>'
+            +'<div class="spa-shell-modal"></div>',
+
+        chat_extend_time    : 1000,
+        chat_retract_time   : 300,
+        chat_extend_height  : 450,
+        chat_retract_height : 15
     },
     stateMap = {$container : null},
     jqueryMap = {},
 
-    setJqueryMap, initModule;
+    setJqueryMap, toggleChat, initModule;
     //---------------モジュールスコープ変数終了---------------
     //-----------------ユーティリティメソッド開始-------------
     //-----------------ユーティリティメソッド終了-------------
@@ -40,21 +45,81 @@ spa.shell = (function () {
     // DOMメソッド/setJqueryMap/開始
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = {$container : $container};
+
+        jqueryMap = {
+            $container : $container,
+            $chat : $container.find('.spa-shell-chat')
+        };
     };
     // DOMメソッド/setJqueryMap終了
+
+    // DOMメソッド/toggleChat開始
+    //目的: チャットスライダーの拡大や収納
+    //引数:
+    //   * do_extend - trueの場合、スライダーを拡大する。falseの場合は格納する。
+    //   * callback - アニメーションに最後に実行するオプションの関数
+    //設定:
+    //   * chat_extend_time, chat_retract_time
+    //   * caht_extend_height, chat_retract_height
+    //戻り値: boolean
+    //   * true - スライダーアニメーションが開始された
+    //   * false - スライダーアニメーションが開始されなかった
+    //
+    toggleChat = function (do_extend, callback) {
+        var
+        px_chat_ht = jqueryMap.$chat.height(),
+        is_open = px_chat_ht === configMap.chat_extend_height,
+        is_closed = px_chat_ht === configMap.chat_retract_height,
+        is_sliding = !is_open && !is_closed;
+
+        //競合状態を避ける
+        if (is_sliding) {return false};
+
+        //チャットスライダーの拡大開始
+        if (do_extend) {
+            jqueryMap.$chat.animate(
+                {height : configMap.chat_extend_height},
+                configMap.chat_extend_time,
+                function () {
+                    if(callback) {callback(jqueryMap.$chat);}
+                }
+            );
+            return true;
+        }
+        //チャットスライダーの拡大終了
+
+        //チャットスライダーの収納開始
+        jqueryMap.$chat.animate(
+            {height : configMap.chat_retract_height},
+            configMap.chat_retract_time,
+            function (){
+                if(callback) {callback(jqueryMap.$chat);}
+            }
+
+        );
+        return true;
+        //チャットスライダーの収納終了
+    };
+    // DOMメソッド/toggleChat終了
     //-----------------DOMメソッド終了-------------
 
     //-----------------イベントハンドラ開始-------------
     //-----------------イベントハンドラ終了-------------
+
     //-----------------パブリックメソッド開始-------------
     //パブリックメソッド /initModule /開始
     initModule = function ($container) {
+        //HTMLをロードし、jQueryコレクションをマッピングする
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
+
+        //切り替えテストをする
+        setTimeout(function () {toggleChat(true);}, 3000);
+        setTimeout(function () {toggleChat(false);}, 8000);
     };
     //パブリックメソッド /initModule /終了
+
     return {initModule : initModule};
     //-----------------パブリックメソッド終了-------------
 }());
