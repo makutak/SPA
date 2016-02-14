b3422583252db6179798e0b85fd11b273eda8068
makutak <kounotakuma@realglobe.jp>
2016-02-15 01:31:14 +0900

    peopleオブジェクトを仕上げた

--- spa/js/spa.model.js
+++ spa/js/spa.model.js
@@ -16,13 +16,60 @@ spa.model = (function () {
     var
     configMap = { anon_id : 'a0' },
     stateMap = {
-        anon_user : null,
+        anon_user      : null,
+        cid_serial     : 0,
         people_cid_map : {},
-        people_db : TAFFY()
+        people_db      : TAFFY(),
+        user           : null
     },
 
     isFakeData = true,
-    personProto, makePerson, people, initModule;
+
+    personProto, makeCid, cleanPeopleDb, completeLogin,
+    makePerson, removePerson, people, initModule;
+
+    //peopleオブジェクトAPI
+    //----------------------
+    //peopleオブジェクトは spa.model.peopleで利用できる
+    //peopleオブジェクトはpersonオブジェクトの集合を管理するためのメソッドと
+    //イベントを提供する。peopleオブジェクトのパブリックメソッドには以下が含まれる。
+    //  * get_user() - 現在のpersonオブジェクトを返す。
+    //    現在のユーザがサインインしていない場合には、匿名personオブジェクトを返す。
+    //
+    //  * get_db() - あらかじめソートされたすべてのpersonオブジェクト
+    //    (現在のユーザを含む)のTaffyデータベースを返す。
+    //
+    //  * get_by_cid(<client_id>) - 指定された一意のIDを持つpersonオブジェクトを返す。
+    //
+    //  * login(<user_name>) - 指定のユーザ名を持つユーザとしてログインする。
+    //    現在のユーザオブジェクトは新しいIDを反映するように変更される。
+    //
+    //  * logout() - 現在のユーザオブジェクトを匿名に戻す。
+    //
+    //このオブジェクトが発行するjQueryグローバルイベントには以下が含まれる。
+    //  * 「spa-login」は、ユーザのログイン処理が完了したときに発行される。
+    //    更新されたユーザオブジェクトをデータとして提供する。
+    //
+    //  * 「spa-logout」はログアウトの完了時に発行される。
+    //    以前のユーザオブジェクトをデータとして提供する。
+    //
+    //それぞれの人はpersonオブジェクトで表される。
+    //personオブジェクトは以下のメソッドを提供する。
+    //  * get_is_user() - オブジェクトが現在のユーザの場合にtrueを返す。
+    //
+    //  * get_is_anon() - オブジェクトが匿名の場合にはtrueを返す。
+    //
+    //personオブジェクトの属性には以下が含まれる。
+    //  * cid - クライアントID文字列。これは常に定義され、
+    //    クライアントデータがバックエンドと同期していない場合のみid属性と異なる。
+    //
+    //  * id - 一意のID。オブジェクトがバックエンドと同期していない場合には未定義になることがある。
+    //
+    //  * name - ユーザの文字列名。
+    //
+    //  * css_map - アバター表現に使う属性のマップ。
+    //
+    //
 
     personProto = {
         get_is_user : function () {
@@ -33,6 +80,33 @@ spa.model = (function () {
         }
     };
 
+    makeCid = function () {
+        return 'c' + String(stateMap.cid_serial++);
+    };
+
+    cleanPeopleDb = function () {
+        var user = stateMap.user;
+        stateMap.people_db = TAFFY();
+        stateMap.people_cid_map = {};
+
+        if (user) {
+            stateMap.people_db.insert(user);
+            stateMap.people_cid_map[user.cid] = user;
+        }
+    };
+
+    completeLogin = function (user_list) {
+        var user_map = user_list[0];
+        delete stateMap.people_cid_map[user_map.cid];
+        stateMap.user.cid = user_map._id;
+        stateMap.user.id  = user_map._id;
+        stateMap.user.css_map = user_map.css_map;
+        stateMap.people_cid_map[user_map._id] = stateMap.user;
+
+        //チャットを追加するときには、ここで参加すべき
+        $.gevent.publish('spa-login', [stateMap.user]);
+    };
+    
     makePerson = function (person_map) {
         var person,
             cid = person_map.cid,
@@ -57,11 +131,71 @@ spa.model = (function () {
         return person;
     };
 
-    people = {
-        get_db : function () { return stateMap.people_db; },
-        get_cid_map : function () { return stateMap.people_cid_map; }
+    removePerson = function (person) {
+        if (!person) { return false; }
+        //匿名ユーザは削除できない
+        if (person.id === configMap.anon_id) {
+            return false;
+        }
+
+        stateMap.people_db({cid : person.cid}).remove();
+        if (person.cid) {
+            delete stateMap.people_cid_map[person.cid];
+        }
+        return true;
     };
 
+    people = (function () {
+        var get_by_cid, get_db, get_user, login, logout;
+
+        get_by_cid = function (cid) {
+            return stateMap.people_cid_map[cid];
+        };
+
+        get_db = function () { return stateMap.people_db; };
+
+        get_user = function () { return stateMap.user; };
+
+        login = function (name) {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+            stateMap.user = makePerson({
+                cid     : makeCid(),
+                css_map : {top : 25, left : 25, 'background-color' : '#8f8'},
+                name    : name
+            });
+
+            sio.on('userupdata', completeLogin);
+
+            sio.emit('adduser', {
+                cid     : stateMap.user.cid,
+                css_map : stateMap.user.css_map,
+                name    : stateMap.user.name
+                });
+        };
+
+        logout = function () {
+            var is_removed, user = stateMap.user;
+            //チャットを追加するときには、ここでチャットルームから出るべき
+
+            is_removed = removePerson(user);
+            stateMap.user = stateMap.anon_user;
+
+            $.gevent.publish('spa-logout', [user]);
+            return is_removed;
+        };
+
+        return {
+            get_by_cid : get_by_cid,
+            get_db     : get_db,
+            get_user   : get_user,
+            login      : login,
+            logout     : logout
+        };
+
+    } () );
+
+              
     initModule = function () {
         var i, people_list, person_map;
 
@@ -71,6 +205,7 @@ spa.model = (function () {
             id   : configMap.anon_id,
             name : 'anonymous'
         });
+        stateMap.user = stateMap.anon_user;
 
         if (isFakeData) {
             people_list = spa.fake.getPeopleList();
@@ -91,47 +226,4 @@ spa.model = (function () {
         people     : people
     };
 }());
-
-//peopleオブジェクトAPI
-//----------------------
-//peopleオブジェクトは spa.model.peopleで利用できる
-//peopleオブジェクトはpersonオブジェクトの集合を管理するためのメソッドと
-//イベントを提供する。peopleオブジェクトのパブリックメソッドには以下が含まれる。
-//  * get_user() - 現在のpersonオブジェクトを返す。
-//    現在のユーザがサインインしていない場合には、匿名personオブジェクトを返す。
-//
-//  * get_db() - あらかじめソートされたすべてのpersonオブジェクト
-//    (現在のユーザを含む)のTaffyデータベースを返す。
-//
-//  * get_by_cid(<client_id>) - 指定された一意のIDを持つpersonオブジェクトを返す。
-//
-//  * login(<user_name>) - 指定のユーザ名を持つユーザとしてログインする。
-//    現在のユーザオブジェクトは新しいIDを反映するように変更される。
-//
-//  * logout() - 現在のユーザオブジェクトを匿名に戻す。
-//
-//このオブジェクトが発行するjQueryグローバルイベントには以下が含まれる。
-//  * 「spa-login」は、ユーザのログイン処理が完了したときに発行される。
-//    更新されたユーザオブジェクトをデータとして提供する。
-//
-//  * 「spa-logout」はログアウトの完了時に発行される。
-//    以前のユーザオブジェクトをデータとして提供する。
-//
-//それぞれの人はpersonオブジェクトで表される。
-//personオブジェクトは以下のメソッドを提供する。
-//  * get_is_user() - オブジェクトが現在のユーザの場合にtrueを返す。
-//
-//  * get_is_anon() - オブジェクトが匿名の場合にはtrueを返す。
-//
-//personオブジェクトの属性には以下が含まれる。
-//  * cid - クライアントID文字列。これは常に定義され、
-//    クライアントデータがバックエンドと同期していない場合のみid属性と異なる。
-//
-//  * id - 一意のID。オブジェクトがバックエンドと同期していない場合には未定義になることがある。
-//
-//  * name - ユーザの文字列名。
-//
-//  * css_map - アバター表現に使う属性のマップ。
-//
-//
 
