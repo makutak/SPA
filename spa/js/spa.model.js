cd5075865496312dbcaa02fd3819af2653d89a49
makutak <kounotakuma@realglobe.jp>
2016-02-21 05:53:33 +0900

    不要なので

--- spa/js/spa.model.js
+++ spa/js/spa.model.js
@@ -185,7 +185,7 @@ spa.model = (function () {
             clearPeopleDb();
 
             $.gevent.publish('spa-logout', [user]);
-            return is_removed;
+            //return is_removed;
         };
 
         return {
