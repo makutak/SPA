927a573ac51ff90213528929081d31ef5a12d98d
makutak <kounotakuma@realglobe.jp>
2016-02-23 01:58:30 +0900

    簡単なロギングの追加

--- nodejs_tutorial/webapp/app.js
+++ nodejs_tutorial/webapp/app.js
@@ -16,8 +16,10 @@ var http, server;
 
 http = require('http');
 server = http.createServer(function (request, response) {
+    console.log(request);
     response.writeHead(200, {'Content-type': 'text/plain'} );
     response.end('Hello World');
 }).listen(3000);
 
 console.log('Listening on port %d', server.address().port);
+
