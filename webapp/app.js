d1fec4c01e2b57f72b862ecfad61448f5eab827e
makutak <kounotakuma@realglobe.jp>
2016-02-25 01:21:46 +0900

    ディレクトリ構成変更

---
+++ webapp/app.js
@@ -0,0 +1,40 @@
+/*
+ * app.js - Hello World
+ */
+
+/* jslint        node   : true, continue : true,
+   devel  : true, indent : true, maxerr   : 50,
+   newcap : true, nomen  : true, plusplus : true,
+   regexp : true, sloppy : true, vars     : false,
+   white  : true
+ */
+
+/* global */
+
+//モジュールスコープ変数の宣言
+'use strict';
+var http = require('http'),
+    express = require('express'),
+    app = express(),
+    server = http.createServer(app);
+//モジュールスコープ変数終了
+
+//サーバ構成開始
+app.configure( function () {
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+
+app.get('/', function(request, response) {
+    response.send('Hello Express');
+});
+//サーバ構成終了
+
+//サーバ起動開始
+server.listen(3000);
+console.log(
+    'Express Server listning on port %d in %s mode',
+    server.address().port, app.settings.env
+);
+//サーバ起動終了
