e67f0683258b4aa083517a198b5f4a6aeb72207a
makutak <kounotakuma@realglobe.jp>
2016-02-25 01:38:06 +0900

    カッコの前後にスペース挿入と、Expressで開発環境、本番環境をサポートするように

--- webapp/app.js
+++ webapp/app.js
@@ -13,26 +13,40 @@
 
 //モジュールスコープ変数の宣言
 'use strict';
-var http = require('http'),
-    express = require('express'),
+var http = require( 'http' ),
+    express = require( 'express' ),
     app = express(),
-    server = http.createServer(app);
+    server = http.createServer( app );
 //モジュールスコープ変数終了
 
 //サーバ構成開始
 app.configure( function () {
-    app.use(express.logger());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    app.use( express.bodyParser() );
+    app.use( express.methodOverride() );
+} );
+
+
+app.configure( 'development', function () {
+    app.use( express.logger() );
+    app.use( express.errorHandler({
+        dumpExceptions :true,
+        showStack : true
+    }) );
 });
 
-app.get('/', function(request, response) {
-    response.send('Hello Express');
+
+app.configure( 'production', function () {
+    app.use( express.errorHandler() );
+});
+
+
+app.get( '/', function( request, response ) {
+    response.send( 'Hello Express' );
 });
 //サーバ構成終了
 
 //サーバ起動開始
-server.listen(3000);
+server.listen( 3000 );
 console.log(
     'Express Server listning on port %d in %s mode',
     server.address().port, app.settings.env
