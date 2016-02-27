2bc7753ae34388eb5aef4eab0dd92e2b811b6713
makutak <kounotakuma@realglobe.jp>
2016-02-28 05:35:33 +0900

    ルートを汎用化した

--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - 高度なルーティングを備えたExpressサーバ
+ * app.js - 汎用ルーティングを備えたExpressサーバ
  */
 
 /* jslint        node   : true, continue : true,
@@ -47,44 +47,42 @@ app.get( '/', function( request, response ) {
 });
 
 
-app.all( '/user/*?', function( request, response, next) {
+app.all( '/:obj_type/*?', function( request, response, next) {
     response.contentType( 'json' );
     next();
 });
 
 
-app.get( '/user/list', function( request, response ) {
-    //response.contentType( 'json' );
-    response.send( {title: 'user list'} );
+app.get( '/:obj_type/list', function( request, response ) {
+    response.send( {title: request.params.obj_type + ' list'} );
 });
 
 
-app.post( '/user/create', function( request, response ) {
-    //response.contentType( 'json' );
-    response.send( {title: 'user created'} );
+app.post( '/:obj_type/create', function( request, response ) {
+    response.send( {title: request.params.obj_type + ' created'} );
 });
 
 
-app.get( '/user/read/:id([0-9]+)', function( request, response ) {
-    //response.contentType( 'json' );
+app.get( '/:obj_type/read/:id([0-9]+)', function( request, response ) {
     response.send({
-        title: 'user with id ' + request.params.id + ' found'
+        title: request.params.obj_type
+            + ' with id ' + request.params.id + ' found'
     });
 });
 
 
-app.post( '/user/update/:id([0-9]+)', function( request, response ) {
-    //response.contentType( 'json' );
+app.post( '/:obj_type/update/:id([0-9]+)', function( request, response ) {
     response.send({
-        title: 'user with id ' + request.params.id + ' updated' 
+        title: request.params.obj_type
+            + ' with id ' + request.params.id + ' updated' 
     });
 });
 
 
-app.get( '/user/delete/:id([0-9]+)', function( request, response ) {
-    //response.contentType( 'json' );
+app.get( '/:obj_type/delete/:id([0-9]+)', function( request, response ) {
     response.send({
-        title: 'user with id ' + request.params.id + ' deleted'
+        title: request.params.obj_type
+            + ' with id ' + request.params.id + ' deleted'
     });
 });
 //サーバ構成終了
