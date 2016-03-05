81fe5de7966b71ace6ebf9b7c67ca0e300ac1d6c
makutak <kounotakuma@realglobe.jp>
2016-03-05 16:49:04 +0900

    作成と更新のルートに検証を追加

--- webapp/routes.js
+++ webapp/routes.js
@@ -19,11 +19,11 @@ var loadSchema, checkSchema, configRoutes,
     JSV      = require('JSV').JSV,
 
     mongoServer = new mongodb.Server(
-        'localhost',
-        mongodb.Connection.DEFAULT_PORT
+      'localhost',
+      mongodb.Connection.DEFAULT_PORT
     ),
     dbHandle = new mongodb.Db(
-        'spa', mongoServer, { safe : true }
+      'spa', mongoServer, { safe : true }
     ),
     validator = JSV.createEnviroment(),
 
@@ -33,128 +33,157 @@ var loadSchema, checkSchema, configRoutes,
 
 //ユーティリティメソッド開始
 loadSchema = function ( schema_name, schema_path ) {
-    fsHandle.readFile( schema_path, 'utf-8', function ( err, data ) {
-        objTypeMap[schema_name] = JSON.parse(data);
-    });
+  fsHandle.readFile( schema_path, 'utf-8', function ( err, data ) {
+    objTypeMap[schema_name] = JSON.parse(data);
+  });
 };
 
 checkSchema = function ( obj_type, obj_map, callback ) {
-    var schema_map = objTypeMap[obj_type],
-        report_map = validator.validate( obj_map, schema_map );
+  var schema_map = objTypeMap[obj_type],
+      report_map = validator.validate( obj_map, schema_map );
 
-    callback( report_map.errors );
+  callback( report_map.errors );
 };
 //ユーティリティメソッド終了
 
 //パブリックメソッド開始
 configRoutes = function (app, server) {
-    app.get( '/', function( request, response ) {
-        response.redirect( '/spa.html' );
-    });
+  app.get( '/', function( request, response ) {
+    response.redirect( '/spa.html' );
+  });
 
-    app.all( '/:obj_type/*?', function( request, response, next) {
-        response.contentType( 'json' );
-        if (objTypeMap[request.params.obj_type]) {
-            next();
+  app.all( '/:obj_type/*?', function( request, response, next) {
+    response.contentType( 'json' );
+    if (objTypeMap[request.params.obj_type]) {
+      next();
+    }
+    else {
+      response.send({ error_msg : request.params.obj_type + ' is not a valid object type'});
+    }
+  });
+
+
+  app.get( '/:obj_type/list', function( request, response ) {
+    dbHandle.collection(
+      request.params.obj_type,
+      function( outer_error, collection ){
+        collection.find().toArray(
+          function(inner_error, map_list) {
+            response.send(map_list);
+          }
+        );
+      }
+    );
+  });
+
+
+  app.post( '/:obj_type/create', function( request, response ) {
+    var obj_type = request.params.obj_type,
+        obj_map  = request.body;
+
+    checkSchema(
+      obj_type, obj_map,
+      function ( error_list ) {
+        if ( error_list.length === 0 ){
+          dbHandle.collection(
+            obj_type,
+            function( outer_error, collection ) {
+              var options_map = {safe : true};
+
+              collection.insert(
+                obj_map,
+                options_map,
+                function ( inner_error, result_map ) {
+                  response.send( result_map );
+                }
+              );
+            }
+          );
         }
         else {
-            response.send({ error_msg : request.params.obj_type + ' is not a valid object type'});
+          response.send({
+            error_msg  : 'Input document not valid',
+            error_list : error_list
+          });
         }
-    });
+      }
+    );
+  });
 
 
-    app.get( '/:obj_type/list', function( request, response ) {
-        dbHandle.collection(
-            request.params.obj_type,
-            function( outer_error, collection ){
-                collection.find().toArray(
-                    function(inner_error, map_list) {
-                        response.send(map_list);
-                    }
-                );
-            }
+  app.get( '/:obj_type/read/:id', function( request, response ) {
+    var find_map = { _id: makeMongoId( request.params.id ) };
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        collection.findOne(
+          find_map,
+          function ( inner_error, result_map ) {
+            response.send( result_map );
+          }
         );
-    });
+      }
+    );
+  });
 
 
-    app.post( '/:obj_type/create', function( request, response ) {
-        dbHandle.collection(
-            request.params.obj_type,
-            function( outer_error, collection ) {
-                var options_map = {safe : true},
-                    obj_map     = request.body;
+  app.post( '/:obj_type/update/:id', function( request, response ) {
+    var find_map = { _id: makeMongoId (request.params.id) },
+        obj_map  = request.body,
+        obj_type =  request.params.obj_type;
 
-                collection.insert(
-                    obj_map,
-                    options_map,
-                    function ( inner_error, result_map ) {
-                        response.send( result_map );
-                    }
-                );
-            }
-        );
-    });
-
-
-    app.get( '/:obj_type/read/:id', function( request, response ) {
-        var find_map = { _id: makeMongoId( request.params.id ) };
-        dbHandle.collection(
-            request.params.obj_type,
+    checkSchema(
+      obj_type, obj_map,
+      function ( error_list ) {
+        if (error_list.length === 0) {
+          dbHandle.collection(
+            obj_type,
             function ( outer_error, collection ) {
-                collection.findOne(
-                    find_map,
-                    function ( inner_error, result_map ) {
-                        response.send( result_map );
-                    }
-                );
+              var sort_order = [],
+                  options_map = {
+                    'new' : true, upsert: false, safe: true
+                  };
+              collection.findAndModify(
+                find_map,
+                sort_order,
+                obj_type,
+                options_map,
+                function ( inner_error, update_map ) {
+                  response.send( update_map );
+                }
+              );
             }
+          );
+        }
+        else {
+          response.send({
+            error_msg  : 'Input document not valid',
+            error_list : error_list
+          });
+        }
+      }
+    );
+  });
+
+
+  app.get( '/:obj_type/delete/:id', function( request, response ) {
+    var find_map = { _id: makeMongoId( request.params.id) };
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        var options_map = {safe: true, single: true};
+
+        collection.remove(
+          find_map,
+          options_map,
+          function ( inner_error, delete_count ) {
+            response.send( { delete_count: delete_count } );
+          }
         );
-    });
-
-
-    app.post( '/:obj_type/update/:id', function( request, response ) {
-        var find_map = { _id: makeMongoId (request.params.id) },
-            obj_type = request.body;
-
-        dbHandle.collection(
-            request.params.obj_type,
-            function ( outer_error, collection ) {
-                var sort_order = [],
-                    options_map = {
-                        'new' : true, upsert: false, safe: true
-                    };
-                collection.findAndModify(
-                    find_map,
-                    sort_order,
-                    obj_type,
-                    options_map,
-                    function ( inner_error, update_map ) {
-                        response.send( update_map );
-                    }
-                );
-            }
-        );
-    });
-
-
-    app.get( '/:obj_type/delete/:id', function( request, response ) {
-        var find_map = { _id: makeMongoId( request.params.id) };
-
-        dbHandle.collection(
-            request.params.obj_type,
-            function ( outer_error, collection ) {
-                var options_map = {safe: true, single: true};
-
-                collection.remove(
-                    find_map,
-                    options_map,
-                    function ( inner_error, delete_count ) {
-                        response.send( { delete_count: delete_count } );
-                    }
-                );
-            }
-        );
-    });
+      }
+    );
+  });
 };
 
 module.exports = {configRoutes : configRoutes};
@@ -162,17 +191,17 @@ module.exports = {configRoutes : configRoutes};
 
 //モジュール初期化開始
 dbHandle.open( function () {
-    console.log( '** Connected to MongoDB ** ' );
+  console.log( '** Connected to MongoDB ** ' );
 });
 
 //スキーマをメモリ(objTypeMap)にロードする
 (function () {
-    var schema_name, schema_path;
-    for (schema_name in objTypeMap) {
-        if ( objTypeMap.hasOwnProperty(schema_name) ) {
-            schema_path = __dirname + '/' + schema_name + '.json';
-            loadSchema(schema_name, schema_path);
-        }
+  var schema_name, schema_path;
+  for (schema_name in objTypeMap) {
+    if ( objTypeMap.hasOwnProperty(schema_name) ) {
+      schema_path = __dirname + '/' + schema_name + '.json';
+      loadSchema(schema_name, schema_path);
     }
+  }
 }());
 //モジュール初期化終了
