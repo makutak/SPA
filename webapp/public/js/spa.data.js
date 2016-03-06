532d12d40818a161f5e302420598f046542a1fc5
makutak <kounotakuma@realglobe.jp>
2016-03-07 02:12:11 +0900

    インデント修正

--- webapp/public/js/spa.data.js
+++ webapp/public/js/spa.data.js
@@ -12,36 +12,36 @@
 
 /* global $, io, spa */
 spa.data = (function () {
-    'use strict';
+  'use strict';
 
-    var stateMap = {sio : null},
-        makeSio, getSio, initModule;
+  var stateMap = {sio : null},
+      makeSio, getSio, initModule;
 
-    makeSio = function () {
-        var socket = io.connect('/chat');
-
-        return {
-            emit : function (event_name, data) {
-                socket.emit(event_name, data);
-            },
-
-            on : function (event_name, callback) {
-                socket.on(event_name, function(){
-                    callback(arguments);
-                });
-            }
-        };
-    };
-
-    getSio = function () {
-        if (!stateMap.sio) { stateMap.sio = makeSio(); }
-        return stateMap.sio;
-    };
-
-    initModule = function () {};
+  makeSio = function () {
+    var socket = io.connect('/chat');
 
     return {
-        getSio     : getSio,
-        initModule : initModule
+      emit : function (event_name, data) {
+        socket.emit(event_name, data);
+      },
+
+      on : function (event_name, callback) {
+        socket.on(event_name, function(){
+          callback(arguments);
+        });
+      }
     };
+  };
+
+  getSio = function () {
+    if (!stateMap.sio) { stateMap.sio = makeSio(); }
+    return stateMap.sio;
+  };
+
+  initModule = function () {};
+
+  return {
+    getSio     : getSio,
+    initModule : initModule
+  };
 } () );
